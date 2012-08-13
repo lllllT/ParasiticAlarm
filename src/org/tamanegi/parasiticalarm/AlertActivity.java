969c63965dbf46a5a7dd56d9460fbb1b58f2903c
lllllT <lllllT@tamanegi.org>
2012-08-13 21:11:50 +0900

    for keyguard after screen off

--- src/org/tamanegi/parasiticalarm/AlertActivity.java
+++ src/org/tamanegi/parasiticalarm/AlertActivity.java
@@ -66,7 +66,6 @@ public class AlertActivity extends Activity
 
         getWindow().addFlags(
             WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
-            WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
             WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
             WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |
             WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON);
